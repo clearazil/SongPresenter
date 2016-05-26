568f2f32416d005772f777acfea78fd5fedaefa6
Derk van der Heide <derkvanderheide@hotmail.com>
2016-05-26 16:41:59 +0200

    added songs index

--- src/AppBundle/Controller/SongsController.php
+++ src/AppBundle/Controller/SongsController.php
@@ -8,9 +8,37 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\Type\SongType;
 use AppBundle\Entity\Song;
 use AppBundle\Entity\SongVerse;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SongsController extends Controller
 {
+    public function indexAction(Request $request)
+    {
+        $songs = null;
+
+        $form = $this->createFormBuilder()
+            ->setMethod('POST')
+            ->add('song_group', EntityType::class, [
+                'class' => 'AppBundle:SongGroup',
+                'choice_label' => 'name'
+            ])
+            ->add('submit', SubmitType::class, ['label' => 'Submit'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $songs = $this->getDoctrine()->getRepository('AppBundle:Song')
+                ->findBy(['song_group' => $request->get('form')['song_group']]);
+        }
+
+        return $this->render('songs/index.html.twig', [
+            'form' => $form->createView(),
+            'songs' => $songs,
+        ]);
+    }
+
     public function createAction(Request $request)
     {
         $song = new Song();
