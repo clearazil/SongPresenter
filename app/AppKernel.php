91c70ac90b76b042aee82e7e435a9166540abe2c
Derk van der Heide <derkvanderheide@hotmail.com>
2016-06-07 21:36:50 +0200

    installed besimple/i18n-routing-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new BeSimple\I18nRoutingBundle\BeSimpleI18nRoutingBundle(),
             new AppBundle\AppBundle(),
         ];
 
